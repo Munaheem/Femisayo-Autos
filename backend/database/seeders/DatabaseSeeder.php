a885df28defbab8746df558c99fc8f97b5ac861f
Yusom-Tech <yunusomomoh@yahoo.com>
2026-09-23 00:13:43 +0100

    Include parts in database seeding

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ServiceSeeder::class,
             TechnicianSeeder::class,
+            PartSeeder::class,
         ]);
     }
 }
